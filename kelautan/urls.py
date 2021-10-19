0547e7fd7231bd070754036f5bca79d47a7a4213
ukasah <90326881+ukasah@users.noreply.github.com>
2021-10-19 21:23:21 +0700

    Delete kelautan directory

--- kelautan/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from sik.views import halamanbaru, halamanlainnya
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('halamanbaru/', halamanbaru),
-    path('halamanlainnya/', halamanlainnya),
-]
